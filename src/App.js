3aa5fc7465cabe7ebbd2cfba80afb734c4289656
djogo16 <kabala.frank@gmail.com>
2018-10-04 22:18:25 -0600

    customize appearance of app with snapshot values

--- src/App.js
+++ src/App.js
@@ -6,7 +6,29 @@ import {DragDropContext} from 'react-beautiful-dnd';
 
 class App extends Component {
   state = initialData
-  onDragEnd = ()=>{}
+  onDragEnd = (result)=>{
+    const { source, destination, draggableId} = result;
+    if(!destination){
+      return;
+    }
+    if(source.droppableId === destination.droppableId && source.index === destination.index){
+      return;
+    }
+    const column = this.state.columns[source.droppableId];
+    const newTasksIds = Array.from(column.taskIds);
+    newTasksIds.splice(source.index,1);
+    newTasksIds.splice(destination.index,0,draggableId);
+    const newColumn = {...column ,taskIds:newTasksIds};
+    const newState = {
+      ...this.state,
+      columns : {
+        ...this.state.columns,
+        [newColumn.id] : newColumn
+      }
+    };
+    this.setState(newState);
+
+  }
   render() {
     return (
       <DragDropContext onDragEnd = {this.onDragEnd}>{
